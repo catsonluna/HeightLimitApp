9583cab1827255c6ff925d29e83d7d625bae229a
pinkulu <kristersj7@gmail.com>
2021-04-16 15:16:33 +0300

    added a way to log locraws and parse them

--- public/main.js
+++ public/main.js
@@ -44,17 +44,32 @@ const paths = {
 	"vanilla": `${homeDirectory}/AppData/Roaming/.minecraft/logs/latest.log`,
 	"badlion": `${homeDirectory}/AppData/Roaming/.minecraft/logs/blclient/minecraft/latest.log`
 }
+var firstTime = true;
 ipcMain.on('GetVanilla', () => {
-	// fileNames is an array that contains all the selected
-	fs.readFile(paths["vanilla"], 'utf-8', (err, data) => {
-		if (err) {
-			alert("An error ocurred reading the file :" + err.message);
-			return;
+	console.log("This Is Getting Called")
+	var currentClient = "vanilla";
+	var file_fd = fs.openSync(`${paths[currentClient]}`, 'r');
+	setInterval(readLogFile, 10);
+	function readLogFile() {
+		if (!file_fd || currentClient == "NONE") { return; }
+		var newSize = fs.fstatSync(file_fd).size;
+		if (firstTime) {
+			lastSize = newSize;
+			firstTime = false;
+		} else {
+			fs.read(file_fd, Buffer.alloc(2056), 0, 2056, lastSize, (err, bytes, buffer) => {
+				lastSize += bytes;
+				const lines = buffer.toString('latin1').split(/\r?\n/).slice(0, -1);
+				lines.forEach(line => {
+				if(line.includes(`[CHAT] {`))
+					{
+						var obj = JSON.parse(line.split("[CHAT]")[1]);
+						console.log(obj.gametype)
+					}
+				});
+			});
 		}
-
-		// Change how to handle the file content
-		console.log("The file content is : " + data);
-	});
+	}
 });
 
 ipcMain.on('GetLunar', () => {
